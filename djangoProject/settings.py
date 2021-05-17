c83aedbd8f33716660dc660613c896226eba46b6
ibk2510 <ibrahimkalil.18cs@kcct.ac.in>
2021-05-17 18:35:08 +0530

    heroku db config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -15,6 +15,7 @@ import os
 import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -178,3 +179,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
